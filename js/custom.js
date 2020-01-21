624a9641b9a47fa954188e713f953853d4df9d96
Justin Jim <justinjim@users.noreply.github.com>
2020-01-21 16:10:01 -0500

    changes
    1

--- js/custom.js
+++ js/custom.js
@@ -2,15 +2,30 @@
 
 function myMove() {
   var elem = document.getElementById("myAnimation");
+  document.getElementById('myContainer').removeAttribute("onclick");
   var pos = 0;
-  var id = setInterval(frame, 10);
+  var id = setInterval(frame, 5);
+  var direction = true;
   function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      //elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
-    }
+      
+      if (pos < 333 && direction == true){
+
+        pos ++;
+
+
+        elem.style.left = pos + 'px';
+      }
+        else{
+          pos --;
+          direction = false;
+          elem.style.left = pos + 'px';
+
+
+        }
+
+      if (pos == 0 & direction == false){
+        direction = true;
+      }
+    
   }
 }
