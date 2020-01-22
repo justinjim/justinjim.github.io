02dfb75a80e104671928ad103468a30348e79545
Justin Jim <justinjim@users.noreply.github.com>
2020-01-21 23:01:51 -0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -18,6 +18,11 @@ function draw() {
   var mouseisDown = false;
   var paddleX = (canvas.width-75)/2;
   var mouseX = 0;
+  var ballRadius = 10;
+
+  var dx = 2;
+var dy = -2;
+
   function mousedownHandler(e){
 
     mouseisDown = true;
@@ -47,14 +52,25 @@ function draw() {
 
   var pos = 0;
   var direction = true;
+
+  var x = canvas.width/2;
+  var y = 0 + ballRadius;
   function drawPaddle() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      //ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       ctx.beginPath();
       ctx.rect(paddleX, 100, 75, 10);
       ctx.fillStyle = "#0095DD";
       ctx.fill();
       ctx.closePath();
+  }
+
+  function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#009533";
+    ctx.fill();
+    ctx.closePath();
   }
 
 
@@ -79,10 +95,32 @@ function draw() {
             paddleX = 0;
         }
     }
-
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     drawPaddle();
-      
 
+if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if(y + dy < ballRadius) {
+    dy = -dy;
+  }
+  else if(y + dy > canvas.height) {
+    if(x > paddleX && x < paddleX + 20) {
+      dy = -dy;
+    }
+  
+      else {
+        x = canvas.width/2;
+        y = canvas.height-30;
+        dx = 3;
+        dy = -3;
+  
+    }
+  }
+    y += dy;
+    x += dx;
+    
 
     //square stuff      
     if (pos < (screen.width - 50) && direction == true){
